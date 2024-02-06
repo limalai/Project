7dcd15d857b393fd7519f3f30a5b6ffd0faed9b5
limalai <limalai.p@kkumail.com>
2024-02-07 00:40:47 +0700

    change They are coming game name folder

--- WorkListWebsite/WorkListProject/src/main/java/com/project/Repository/SubjectDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.project.table.SubjectDetail;
-
-public interface SubjectDetailRepository extends CrudRepository<SubjectDetail, Integer> {
-
-}
